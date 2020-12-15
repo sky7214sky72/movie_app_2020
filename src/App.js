b77724a1a78bd3416d9a110c1a70a095a42e8679
sky72145 <sky7214sky72@naver.com>
2020-12-15 18:09:49 +0900

    20201215 리액트공부

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React from 'react';
-
-function Food({fav}){  
-  return <h1>I like {fav}</h1>;
-}
-function App() {
-  //return <div className="App"/>
-  return (
-    <div>
-      <h1>Hello!!!!!!</h1>      
-      <Food fav="kimchi"/>  
-      <Food fav="ramen"/>  
-      <Food fav="kimbob"/>
-      <Food fav="kim"/>    
-    </div>
-  );
-}
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
+class App extends React.Component{
+  state = {
+    isLoding: true,
+    movies:[],
+  };
+  getMovies = async () => {
+    const{
+      data:{
+        data:{movies},
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading:false});
+  };
+  componentDidMount(){
+    //영화 데이터 로딩
+    this.getMovies();
+  }
+  render(){
+   const { isLoading,movies } = this.state;
+   return( 
+   <section class="container">
+     {isLoading?(
+      <div class="loader">
+        <span class="loader__text">Loding...</span>
+      </div>
+      ):(
+        <div class="movies">
+          {movies.map(movie => (
+            <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+            />
+          ))}
+        </div>
+      )}
+      </section>
+    );
+  }
+}  
 
 export default App;
